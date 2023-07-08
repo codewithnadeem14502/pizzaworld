2953bb407bdf141e7375c4814ebcba6b097f5eb6
MohdNadeem <codewithnadeem@gmail.com>
2023-07-08 19:57:27 +0530

    add some changes

--- src/component/Pizza.jsx
+++ src/component/Pizza.jsx
@@ -1,18 +1,8 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 
-const Pizza = ({ pizzaobj }) => {
-  return (
-    <>
-      <li className={`pizza  ${pizzaobj.soldOut ? "sold-out" : ""}`}>
-        {pizzaobj.photoName}
-        <div>
-          <h1>{pizzaobj.name}</h1>
-          <h1>{pizzaobj.ingredients}</h1>
-          <span>{pizzaobj.soldOut ? "SOLD-OUT" : pizzaobj.price * 10}</span>
-        </div>
-      </li>
-    </>
-  );
+const Pizza = () => {
+  return <div></div>;
 };
 
 export default Pizza;
